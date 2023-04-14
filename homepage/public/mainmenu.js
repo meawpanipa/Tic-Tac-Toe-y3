49e8d79637533771355e5552c281015d04c2bc49
IntanonTangtung <73689440+IntanonTangtung@users.noreply.github.com>
2023-04-14 23:26:16 +0700

    Add files via upload

---
+++ homepage/public/mainmenu.js
@@ -0,0 +1,27 @@
+
+
+let readList = () => {
+    var ref = firebase.database().ref("users");
+    ref.once("value").then((snapshot) => {
+        snapshot.forEach((data) => {
+            var id = data.key;
+            ref.once("value").then((snapshot) => {
+                let username = snapshot.child(id).child("username").val();
+                let winNum = snapshot.child(id).child("winRound").val();
+                let answerNum = snapshot.child(id).child("answer").val();
+                let correctNum = snapshot.child(id).child("correctNum").val();
+                let round = snapshot.child(id).child("round").val();
+                let score = snapshot.child(id).child("score").val();
+
+                document.getElementById("username").innerHTML = username;
+                document.getElementById("win_rate").innerHTML = (winNum*round)/100;
+                document.getElementById("correct_rate").innerHTML = (answerNum*correctNum)/100;
+                document.getElementById("player_score").innerHTML = score;
+                document.getElementById("winNum").innerHTML = (winNum*round)/100;
+                document.getElementById("questionNum").innerHTML = answerNum;
+            })
+        })
+    })
+}
+
+window.onload = readList;
